eb535728f37b203ae94231948e2e4e6f5a7a78e4
aleh.belski <aleh.belski@absoftware.dev>
2026-06-07 15:56:01 +0200

    feat: surface

---
+++ bar/include/surface.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "wlr-layer-shell-unstable-v1-client-protocol.h"
+
+class Surface {
+public:
+    struct Dimensions {
+        int bar_width;
+        int bar_height;
+    };
+
+private:
+    //----
+    // SURFACE = canvas. Empty rect recognized by COMPOSITOR.
+    // w/o SURFACE there is nothing - no pos, no size, no pixels
+    wl_surface *surface;
+
+    // LAYER_SURFACE - extension to surface specific for wlroots.
+    // It tells COMPOSITOR that we have layer object instead of regular window
+    // nullptr - output (screen), nullptr as arg in our case means COMPOSITOR
+    // will pick itself ZWLR_LAYER_SHELL_V1_LAYER_TOP - layer above regular
+    // windows, below overlays. "bar" - namespace name, serves as id
+    zwlr_layer_surface_v1 *layer_surface;
+
+    // TODO: width comes from configure callback - `layer_surface_configure_cb`
+    // for now hardcode
+    Dimensions dimensions;
+
+    // COMPOSITOR sends `configure` with real sizes when ready to show
+    // `surface`.
+    static void layer_surface_configure_cb(void *data,
+                                           zwlr_layer_surface_v1 *layer_surface,
+                                           uint32_t serial, uint32_t width,
+                                           uint32_t height);
+
+    // COMPOSITOR sends `closed` when surface will be removed,
+    // .e.g display turned off
+    static void layer_surface_closed_cb(void *data,
+                                        zwlr_layer_surface_v1 *layer_surface);
+    const zwlr_layer_surface_v1_listener layer_surface_listener = {
+        .configure = layer_surface_configure_cb,
+        .closed = layer_surface_closed_cb,
+    };
+
+public:
+    Surface(wl_compositor *compositor, zwlr_layer_shell_v1 *layer_shell,
+            int default_w, int default_h);
+
+    void commit(wl_buffer *buffer);
+    Dimensions get_dimensions() const
+    {
+        return dimensions;
+    }
+};
---
+++ bar/src/surface.cpp
@@ -0,0 +1,61 @@
+#include "surface.hpp"
+
+#include <iostream>
+
+void Surface::layer_surface_configure_cb(void *data,
+                                         zwlr_layer_surface_v1 *layer_surface,
+                                         uint32_t serial, uint32_t width,
+                                         uint32_t height)
+{
+    auto *surface = static_cast<Surface *>(data);
+    zwlr_layer_surface_v1_ack_configure(layer_surface, serial);
+    surface->dimensions.bar_width = width;
+    surface->dimensions.bar_height = height;
+}
+
+void Surface::layer_surface_closed_cb(void *data,
+                                      zwlr_layer_surface_v1 *layer_surface)
+{
+    std::cout << "layer surface closed\n";
+}
+
+Surface::Surface(wl_compositor *compositor, zwlr_layer_shell_v1 *layer_shell,
+                 int default_w, int default_h)
+{
+    dimensions = {
+        .bar_width = default_w,
+        .bar_height = default_h,
+    };
+
+    surface = wl_compositor_create_surface(compositor);
+    layer_surface = zwlr_layer_shell_v1_get_layer_surface(
+        layer_shell, surface, nullptr, ZWLR_LAYER_SHELL_V1_LAYER_TOP, "bar");
+
+    zwlr_layer_surface_v1_set_size(layer_surface, 0, dimensions.bar_height);
+
+    // Anchoring bar to the top
+    // TODO: get from config!
+    zwlr_layer_surface_v1_set_anchor(layer_surface,
+                                     ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP |
+                                         ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT |
+                                         ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT);
+
+    zwlr_layer_surface_v1_set_exclusive_zone(layer_surface,
+                                             dimensions.bar_height);
+
+    zwlr_layer_surface_v1_add_listener(layer_surface, &layer_surface_listener,
+                                       this);
+
+    // Wayland bufferizing all changes and applies them only after commit
+    wl_surface_commit(surface);
+}
+
+void Surface::commit(wl_buffer *buffer)
+{
+    // Telling SURFACE which buffer to show and commit
+    // New pixels will be AVAILABLE for COMPOSER only after commit
+    wl_surface_attach(surface, buffer, 0, 0);
+    wl_surface_damage(surface, 0, 0, dimensions.bar_width,
+                      dimensions.bar_height);
+    wl_surface_commit(surface);
+}
